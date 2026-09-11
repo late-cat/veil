18c64c98b0e5677bcd2d3b3035faa42b1097efb0
Bapi Mondal <bapieyes@gmail.com>
2026-09-11 23:42:06 +0530

    fix: default fallback contract to preprod

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,9 +2,9 @@ export const PREVIEW_CONTRACT_ADDRESS = '';
 export const PREPROD_CONTRACT_ADDRESS = 'f6532d62d3991079b4aea42544ae744ee0d5f3462be8a75c62cbf514ffa5a974';
 
 export const FALLBACK_CONTRACT_ADDRESS =
-  process.env.NEXT_PUBLIC_NETWORK_ID === 'preprod'
-    ? PREPROD_CONTRACT_ADDRESS
-    : PREVIEW_CONTRACT_ADDRESS;
+  process.env.NEXT_PUBLIC_NETWORK_ID === 'preview'
+    ? PREVIEW_CONTRACT_ADDRESS
+    : PREPROD_CONTRACT_ADDRESS;
 
 export const getContractAddress = (): string => {
   if (typeof window !== 'undefined') {
